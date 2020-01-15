90acbc873c3fdd8cfb6f499c9462edf33c619658
Adam Pettigrew <adam@pettigrew.org.uk>
2020-01-15 15:24:35 +0000

    //UCards Implementations Added
    Added all the implementations to TCard

--- UCards.pas
+++ UCards.pas
@@ -25,37 +25,58 @@ implementation
 
 constructor TCard.Create(r, s: integer);
 begin
-
+  rank:=r;
+  suit:=s;
+  orientation:=back;
 end;
 
 function TCard.GetName: string;
 begin
-
+  result:=GetRankAsString + 'of' + GetSuitAsString;
 end;
 
 function TCard.GetOrientation: TOrientation;
 begin
-
+  result:=orientation;
 end;
 
 function TCard.GetRank: integer;
 begin
-
+  result:=rank;
 end;
 
 function TCard.GetRankAsString: string;
 begin
-
+  case Rank of
+    1: result := 'Ace';
+    2: result := 'Two';
+    3: result := 'Three';
+    4: result := 'Four';
+    5: result := 'Five';
+    6: result := 'Six';
+    7: result := 'Seven';
+    8: result := 'Eight';
+    9: result := 'Nine';
+    10: result := 'Ten';
+    11: result := 'Jack';
+    12: result := 'Queen';
+    13: result := 'King';
+  end;
 end;
 
 function TCard.GetSuit: integer;
 begin
-
+  result:=Suit;
 end;
 
 function TCard.GetSuitAsString: String;
 begin
-
+  case Suit of
+    1: result := 'Diamonds';
+    2: result := 'Spades';
+    3: result := 'Hearts';
+    4: result := 'Clubs';
+  end;
 end;
 
 end.
